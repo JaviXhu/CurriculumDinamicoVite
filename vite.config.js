63066a0c2d6721501613198c4828c8c444c4ae8a
JaviXhu <80958170+JaviXhu@users.noreply.github.com>
2024-01-22 18:35:19 +0000

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/', // Configura la ruta base, que es '/' en este caso
-  plugins: [vue()],
-})
